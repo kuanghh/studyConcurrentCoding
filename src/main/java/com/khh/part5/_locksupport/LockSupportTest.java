8877d8d56f8ca13441ad66d797c5cb8e06bbbdac
kuanghaohua <951087952@qq.com>
2019-01-15 17:21:34 +0800

    111

--- src/main/java/com/khh/part5/_locksupport/LockSupportTest.java
+++ src/main/java/com/khh/part5/_locksupport/LockSupportTest.java
@@ -8,8 +8,10 @@ import java.util.concurrent.locks.LockSupport;
  *
  *     LockSupport.park(); 用来阻塞当前线程
  *     LockSupport.unpark(Thread thread)唤醒某个线程
- *     LockSupport.parkNanos(long nanos) 阻塞当前线程，最长不超过nanos纳秒，返回条件在park（）的基础上增加了超时返回
- *     LockSupport.parkUntil(long deadline) 阻塞当前线程，直到deadline时间(从1970年开始到deadline时间的毫秒数)
+ *     LockSupport.parkNanos(long nanos)
+ *          阻塞当前线程，最长不超过nanos纳秒，返回条件在park()的基础上增加了超时返回
+ *     LockSupport.parkUntil(long deadline)
+ *          阻塞当前线程，直到deadline时间(从1970年开始到deadline时间的毫秒数)
  *
  */
 public class LockSupportTest {
@@ -19,9 +21,12 @@ public class LockSupportTest {
         public void run() {
             System.out.println("A run");
             System.out.println("A stop");
-            LockSupport.park();//LockSupport.park(Object blocker),可以用来方便问题排查和系统监控
-//            LockSupport.parkNanos(1000);//LockSupport.parkNanos(Object blocker,long nanos),可以用来方便问题排查和系统监控
-//            LockSupport.parkUntil(System.currentTimeMillis()+1000);//LockSupport.parkUntil(Object blocker,long deadline),可以用来方便问题排查和系统监控
+            LockSupport.park();
+            //LockSupport.park(Object blocker)
+            //LockSupport.parkNanos(1000);
+            //LockSupport.parkNanos(Object blocker,long nanos)
+            //LockSupport.parkUntil(System.currentTimeMillis()+1000);
+            // LockSupport.parkUntil(Object blocker,long deadline)
             System.out.println("A awake");
         }
     }
